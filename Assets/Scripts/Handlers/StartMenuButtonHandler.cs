995d83cc164646a5202b6127788b8f16411e18c9
Agastya Gaur <73049397+agastiyo@users.noreply.github.com>
2022-01-13 09:42:08 -0800

    Button Handler
    Created a Button Handler and updated UI positioning and resolution

---
+++ Assets/Scripts/Handlers/StartMenuButtonHandler.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartMenuButtonHandler : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnNewGame()
+    {
+
+    }
+
+    public void OnLoadGame()
+    {
+
+    }
+
+    public void OnSettings()
+    {
+
+    }
+
+    public void OnCredits()
+    {
+
+    }
+
+    public void OnExitGame()
+    {
+
+    }
+}
