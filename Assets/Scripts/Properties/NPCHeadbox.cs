2891a98dcccbb7cd8771d05f93d096e8a1e4c01e
Agastya Gaur <73049397+agastiyo@users.noreply.github.com>
2022-01-14 12:47:34 -0800

    Code optimization

--- Assets/Scripts/Properties/NPCHeadbox.cs
+++ Assets/Scripts/Properties/NPCHeadbox.cs
@@ -8,25 +8,16 @@ public class NPCHeadbox : MonoBehaviour
     public NPCProfile npcProfile;
     public TextMesh thisHeadbox;
 
-    private string npcName;
-    private string npcProfession;
-
     // Start is called before the first frame update
     void Start()
     {
-        npcName = npcProfile.npcName;
-        npcProfession = npcProfile.npcProfession;
-
-        thisHeadbox.text = $"{npcName}\n-{npcProfession}-";
+        thisHeadbox.text = $"{npcProfile.npcName}\n-{npcProfile.npcProfession}-";
     }
 
     // Update is called once per frame
     void Update()
     {
-        float dist = Vector3.Distance(transform.position, Camera.main.transform.position);
-
         Quaternion rotation = Quaternion.LookRotation(transform.position - Camera.main.transform.position);
-
         transform.rotation = rotation;
     }
 }
