730c26b59ca10267148596666eac9a904932fd89
Agastya Gaur <agastya.gaur@gmail.com>
2022-01-15 20:03:13 -0800

    Dialogue Graph

---
+++ Assets/Scripts/Nodes/DialogueNode.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using XNode;
+
+public class DialogueNode : BaseNode
+{
+    [Input] public int entry;
+    [Output] public int exit;
+    public string npcName;
+    public string npcDialogue;
+
+    public override string GetString()
+    {
+        return $"{npcName} says {npcDialogue}";
+    }
+}
