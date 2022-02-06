8aba4336b3f02e3f14ac8172c784575c4592abe8
Agastya Gaur <agastya.gaur@gmail.com>
2022-02-06 02:43:51 -0800

    Raycast Optimization

--- Assets/Scripts/PlayerCamera/CameraRaycast.cs
+++ Assets/Scripts/PlayerCamera/CameraRaycast.cs
@@ -8,15 +8,13 @@ public class CameraRaycast : MonoBehaviour
 {
     [Range(0f,20f)]
     public float rayDist; //the distance the raycast detects objects
-    public Canvas actionUI;
+    public Canvas actionUI; //the action ui
     
-
     private DialogueHandler dialogueHandler;
     private ActionHandler actionHandler;
     private ItemHandler itemHandler;
 
     private Vector3 center; //center of the screen
-    private bool rayHit; //Did the ray hit something?
     private TextMeshProUGUI actionText; //text in action UI
     private GameObject obj, prevObj; //the object the raycast hit in this/previous frame
     private RaycastHit hit, prevHit; //data of what the raycast hit in this/previous frame
@@ -43,12 +41,11 @@ public class CameraRaycast : MonoBehaviour
     void Update()
     {
         Ray ray = Camera.main.ScreenPointToRay(center);
-        rayHit = Physics.Raycast(ray, out hit, rayDist);
-        Debug.DrawRay(ray.origin, ray.direction * 10, Color.yellow);
+        Physics.Raycast(ray, out hit, rayDist);
 
         obj = hit.transform.gameObject;
 
-        if (rayHit && obj != prevObj) //if the player looks toward a new object
+        if (obj != prevObj) //if the player looks toward a new object
         {
             Debug.Log("This object is different!");
 
@@ -72,25 +69,18 @@ public class CameraRaycast : MonoBehaviour
             }
             else //if the object doesnt matter
             {
-                ResetAll();
+                dialogueHandler.SetCurrentGraph(null);
+                itemHandler.SetCurrentItem(null);
+
+                actionHandler.DisableTalking();
+                actionHandler.DisablePickups();
+
+                actionUI.enabled = false;
+
+                Debug.Log("Graph/Item has been nulled!");
             }
         }
-        else if (rayHit && obj == prevObj) { } //do nothing
-        else { ResetAll(); } //reset everything
 
         prevObj = obj;
-    }
-
-    void ResetAll() 
-    {
-        dialogueHandler.SetCurrentGraph(null);
-        itemHandler.SetCurrentItem(null);
-
-        actionHandler.DisableTalking();
-        actionHandler.DisablePickups();
-
-        actionUI.enabled = false;
-
-        Debug.Log("Graph/Item has been nulled!");
     }
 }
